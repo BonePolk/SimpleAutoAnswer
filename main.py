d1dfc6550beb055ccb5ffd69f6d1bca5b9ec6393
BonePolk <87945722+BonePolk@users.noreply.github.com>
2023-02-21 00:41:11 +0500

    another fraze

--- main.py
+++ main.py
@@ -7,27 +7,31 @@ app = Client("my_account", app_id, app_hash)
 
 enable = False
 
+allready = []
+
 @app.on_message(filters.private and filters.incoming)
 async def mes(client, message):
-    global enable
+    global enable, already
     
     if message.from_user.is_self:
-        print(message)
+        
         if "AS" in message.text:
             enable = False
+            already = []
             await app.send_message(message.chat.id, "Stopped")
         elif "AE" in message.text:
             enable = True
             await app.send_message(message.chat.id, "Started")
     
     elif enable and message.chat.type.value == "private":
-        
-        await app.send_message(message.chat.id, "Я занят")
-        await app.send_message(message.chat.id, "Возможно я вам скоро отвечу")
-        await app.send_message(message.chat.id, "А если не отвечу напишите мне в 19:00 по мск")
-        await app.send_message(message.chat.id, "Либо вообще напишите @...")
-    else:
-        print(enable)
+        if message.from_user.id in already:
+            await app.send_message(message.chat.id, "Я же написал мой хозяин занят")
+        else:
+            already.append(message.from_user.id)
+            await app.send_message(message.chat.id, "Я занят")
+            await app.send_message(message.chat.id, "Возможно я вам скоро отвечу")
+            await app.send_message(message.chat.id, "А если не отвечу напишите мне в 19:00 по мск")
+            await app.send_message(message.chat.id, "Либо вообще напишите @...")
     
     return
     
