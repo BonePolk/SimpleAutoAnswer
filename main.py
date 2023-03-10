e4f2a9e7c354b4cbb299bab8a94e4e1cf10784c1
BonePolk <87945722+BonePolk@users.noreply.github.com>
2023-03-11 00:27:48 +0500

    Added autoread and stop on active chats

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from pyrogram import Client, filters
 import asyncio
 from random import randint
 
+
+BLACKLIST = []
+MIN_DELAY = 1
+MAX_DELAY = 20
+DELAY_DIV = 100
+
 class AutoAnswerClient(Client):
     def __init__(self, str, app_id, app_hash):
         super().__init__(str, app_id, app_hash)
@@ -9,30 +15,48 @@ class AutoAnswerClient(Client):
         self.already = {}
         self.enable = False
         
-        self.blacklist = [1680780422]
-        self.stopfrase = ["ok", "ок", "хорошо", "okey"]
+        self.blacklist = BLACKLIST
+        self.stopfrase = ["окей", "хорошо", "okey", "понятно", "ценю", "пон", "ок", "ok"]
+        
+        self.notread = []
+        
         
 app_id = "28091304"
 app_hash = "bf3863212c54af7a45f20a5e0f8db84e"
 
 app = AutoAnswerClient("my_account", app_id, app_hash)
 
-MIN_DELAY = 1
-MAX_DELAY = 20
-DELAY_DIV = 100
+        
 
-@app.on_message(filters.private and filters.incoming)
+@app.on_message(filters.private)
 async def mes(client, message):
+    
+    if message.outgoing:
+        await handle_outgo(client, message)
+        return
+        
+    if message.chat.id in client.blacklist:
+         return
     
     if message.from_user.is_self:
         
         if "AS" in message.text:
             client.enable = False
             client.already = {}
+            client.blacklist = BLACKLIST
             await client.send_message(message.chat.id, "Stopped")
         elif "AE" in message.text:
             client.enable = True
             await client.send_message(message.chat.id, "Started")
+        elif "AR" in message.text:
+            
+            for cid in client.notread:
+                
+                await client.read_chat_history(cid)
+                
+            client.notread = []
+                
+            await client.send_message(message.chat.id, "I am read all garbage")
         
         return
     
@@ -64,18 +88,23 @@ async def mes(client, message):
         await asyncio.sleep(randint(MIN_DELAY, MAX_DELAY)/DELAY_DIV)
         await client.send_message(message.chat.id, "Возможно он вам скоро ответит просто подождите")
         await asyncio.sleep(randint(MIN_DELAY, MAX_DELAY)/DELAY_DIV)
-        if message.chat.id not in client.blacklist:
-            await client.send_message(message.chat.id, "А если он не ответит, напишите ему в 19:00 по мск скорее всего он будет уже более менее свободен")
-            await asyncio.sleep(randint(MIN_DELAY, MAX_DELAY)/DELAY_DIV)
-            await client.send_message(message.chat.id, "Либо вообще пишите @...")
-            await asyncio.sleep(randint(MIN_DELAY, MAX_DELAY)/DELAY_DIV)
+        await client.send_message(message.chat.id, "А если он не ответит, напишите ему в 19:00 по мск скорее всего он будет уже более менее свободен")
+        await asyncio.sleep(randint(MIN_DELAY, MAX_DELAY)/DELAY_DIV)
+        await client.send_message(message.chat.id, "Либо вообще пишите @...")
+        await asyncio.sleep(randint(MIN_DELAY, MAX_DELAY)/DELAY_DIV)
+        client.notread.append(message.chat.id)
     
     return
     
-
+async def handle_outgo(client, message):
+    if message.chat.id not in client.blacklist:
+        client.blacklist.append(message.chat.id)
+    
+        await message.reply_text("он здесь мой хозяин здесь!", reply_to_message_id=message.id)
 
 
 def main():
+    print("started")
     app.run()
 
 if __name__ == "__main__":
